9b8f079193a202457e5f54760d2470e9c19389e8
Shohan-Pramanik <shohan.pramanik38@gmail.com>
2018-04-26 10:45:06 +0600

    Sum Function

--- GroupPoject.cpp
+++ GroupPoject.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+//sum function
+int sumfunction(int a,int b){
+    int sum;
+    sum=a+b;
+    return sum;
+}
 int main()
 {
+    int x,y,z;
+    cin>>x>>y;
+    z=sumfunction(x,y);
+    cout<<"Sum :"<<z<<endl;
 	return 0;
 }
