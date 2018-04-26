788a0113d8f906cf7496cfc05fd5b08e46c9436c
Ebrahim <ebrahimali.cse.ru@gmail.com>
2018-04-26 10:51:17 +0600

    Multiply

--- GroupPoject.cpp
+++ GroupPoject.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int Multiplication(int Num,int Num2)
+{
+    return Num*Num2;
+}
 int main()
 {
+    int var,var1;
+    cin>>var>>var1;
+   cout<< Multiplication(var,var1);
 	return 0;
 }
